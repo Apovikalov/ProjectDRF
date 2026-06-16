948c2c719b47bc7fe7771b68f3c24b6fefde60e3
Андрей Повикалов <apovikalov@bk.ru>
2026-06-16 22:11:18 +0600

    30.1. Вьюсеты и дженерики

--- config/settings.py
+++ config/settings.py
@@ -10,9 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 from django.conf.global_settings import AUTH_USER_MODEL
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,12 +79,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+load_dotenv()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'projectdrf',
         'USER': 'postgres',
-        'PASSWORD': '12345'
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- materials/serializers.py
+++ materials/serializers.py
@@ -13,4 +13,4 @@ class LessonSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Lesson
-        fields = ('name', 'description', 'image', 'course')
+        fields = ('id', 'name', 'description', 'image', 'course')
