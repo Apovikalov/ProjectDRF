da51d7c4e7e75fb27654035f7084d04425415441
Андрей Повикалов <apovikalov@bk.ru>
2026-07-01 21:37:27 +0600

    Пагинация

--- materials/views.py
+++ materials/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets, generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
 from materials.models import Course, Lesson
+from materials.pagination import MyPagination
 from materials.serializers import CourseSerializer, LessonSerializer
 from users.permissions import GroupCanEditOrReadOnly, IsOwner
 
@@ -48,3 +50,23 @@ class LessonUpdateAPIView(generics.UpdateAPIView):
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
+
+
+class CoursePaginationView(APIView):
+    pagination_class = MyPagination
+
+    def get(self, request):
+        queryset = Course.objects.all()
+        paginated_queryset = self.paginate_queryset(queryset)
+        serializer = CourseSerializer(paginated_queryset, many=True)
+        return self.get_paginated_response(serializer.data)
+
+
+class LessonPaginationView(APIView):
+    pagination_class = MyPagination
+
+    def get(self, request):
+        queryset = Lesson.objects.all()
+        paginated_queryset = self.paginate_queryset(queryset)
+        serializer = LessonSerializer(paginated_queryset, many=True)
+        return self.get_paginated_response(serializer.data)
